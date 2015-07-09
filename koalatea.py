e728d6151f5157c33e8348727355530c038e2d40
Isaac Liao <isaacliao@gmail.com>
2015-07-09 13:52:44 -0700

    Configuration JSON defaults to koalatea.json in same directory as executable; user needs to specify bam and vcf filenames.

--- koalatea.py
+++ koalatea.py
@@ -4,19 +4,23 @@ import subprocess
 import argparse
 import string
 import json
+import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument('inputjson')
+koalatea_dir = os.path.dirname(os.path.realpath(__file__))
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-c', '--conf', help="Input configuration JSON", default=os.path.join(koalatea_dir, "koalatea.json"))
 parser.add_argument('medgapdir')
+parser.add_argument('bamfile')
+parser.add_argument('vcffile')
 args = parser.parse_args()
 args_dict = vars(args)
 
 # Get version from json
-with open(args.inputjson) as jsonfile:
+with open(args.conf) as jsonfile:
     json_dict = json.load(jsonfile)
     args_dict['version'] = json_dict['version']
 
-template_string = 'jsonWorkflow.py -c ${inputjson} -s ${medgapdir}/koalatea-${version}/koalatea.sjm bam=${medgapdir}/genome.recal.bam vcf=${medgapdir}/genome.recal.vcf --outdir ${medgapdir}/koalatea-${version} --run'
+template_string = 'jsonWorkflow.py -c ${conf} -s ${medgapdir}/koalatea-${version}/koalatea.sjm bam=${medgapdir}/${bamfile} vcf=${medgapdir}/${vcffile} --outdir ${medgapdir}/koalatea-${version} --run'
 cmd_template = string.Template(template_string)
 cmd_string = cmd_template.substitute(args_dict)
 subprocess.call(cmd_string, shell=True)
